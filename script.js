63286a5029044b218cd7825ccc4db1b9e327f257
delafor <112673306+delafor@users.noreply.github.com>
2023-10-13 20:11:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function tabuada() { 
+    let num = document.getElementById ('txtn') 
+    let tab = document.getElementById ('seltab')
+      
+    if (num.value.length ==0){ 
+        alert('[ERRO] Por favor! digite um número!')
+    }else{  
+        let n = Number(num.value) 
+        tab.innerHTML = ''
+        for (let c = 1 ;c <= 10; c++) {
+        let item = document.createElement('option') 
+        item.text = `${n} * ${c} = ${n*c}`  
+        tab.appendChild(item) 
+      } 
+      
+    } 
+    
+}   
